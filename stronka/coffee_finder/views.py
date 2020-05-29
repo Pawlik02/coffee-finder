d9c127c42f0a6dd0894192a235eba9c8f686cb64
Pawlik02 <pawelswaj123@gmail.com>
2020-05-29 21:20:55 +0200

    Login handler

--- stronka/coffee_finder/views.py
+++ stronka/coffee_finder/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
-    return render(request,"coffee_finder/index.html")
+    return render(request, "coffee_finder/index.html")
+
 def signup(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
@@ -18,4 +19,15 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request,"coffee_finder/signup.html",{"form":form})
-    
+
+def login_handler(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return render(request, "coffee_finder/index.html")
+        else:
+            return HttpResponse("Wrong data")
+    return render(request, "coffee_finder/login.html")
