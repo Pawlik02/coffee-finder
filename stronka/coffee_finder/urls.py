391aefe78f1a5353b1fde85326fb933200d25a9f
Pawlik02 <pawelswaj123@gmail.com>
2020-05-29 15:35:31 +0200

    First view

--- stronka/coffee_finder/urls.py
+++ stronka/coffee_finder/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "coffee_finder"
 urlpatterns = [
     path("",views.index,name="index")
 ]
--- stronka/coffee_finder/views.py
+++ stronka/coffee_finder/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Test")
+    return render(request,"coffee_finder/index.html")
