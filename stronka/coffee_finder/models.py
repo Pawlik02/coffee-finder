286858bce3a8c27b550d57c80771a703de2341df
Pawlik02 <pawelswaj123@gmail.com>
2020-05-30 23:10:37 +0200

    Location setup

--- stronka/coffee_finder/models.py
+++ stronka/coffee_finder/models.py
@@ -3,3 +3,4 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
+    location = models.CharField(max_length=50,default="Warsaw")
